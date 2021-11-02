c7889dde39e3af3d6329414c4e34850014672671
Yoichi-Maribao <szk.m.yo1@gmail.com>
2021-11-03 02:42:47 +0900

    [complete] 5

--- js-5/app.js
+++ js-5/app.js
@@ -1,11 +1,19 @@
-$(function(){
+$(function () {
   // $('.box1').css({
   //   'background-color': '#0000ff',
   //   'height': '100px'
   // })
-  // $('.box1').slideDown();
   // $('.box1').slideUp();
   // $('.box1').show();
   // $('.box1').css({'background-color': '#0000ff'});
-  $('.box1').hide();
+  // $('.box1').hide();
+  $('.box1').slideDown(function () {
+    $('.box1')
+      .css({
+        'background-color': '#0000ff',
+        height: '100px',
+        width: '200px',
+      })
+      .slideUp();
+  });
 });
