510347f2d964b6ca97ba23742eb86f5d41dcf682
Yoichi-Maribao <szk.m.yo1@gmail.com>
2021-11-03 16:25:09 +0900

    [complete] 6

---
+++ js-6/app.js
@@ -0,0 +1,21 @@
+$(function () {
+  // $('.box1').mouseover(() => {
+  // $('.box1').css({
+  //   'background-color': '#0000ff',
+  // });
+  // $('.box1').addClass('box1-ext');
+  // });
+  // $('.box1').mouseout(() => {
+  // $('.box1').css({
+  //   'background-color': '#ff0000',
+  // });
+  // $('.box1').removeClass('box1-ext');
+  // });
+
+  $('.box1').on('click', () => {
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(() => {
+    $('.box1').removeClass('box1-ext');
+  });
+});
